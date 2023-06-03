2baf4b0679b214775097dbb184848383a889a12f
Nickpodski <Nickpodski@gmail.com>
2023-06-03 17:30:06 -0400

    Removing BsonElement from models with a few updates

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,15 +5,12 @@ namespace ReelJunkies.Models;
 
 public class Movie 
 {
-    [BsonElement("Title")]
+
     public string Title { get; set; } = null!;
 
-    [BsonElement("MovieId")]
     public int MovieId { get; set; }
 
-    [BsonElement("PassWord")]
-    public Array PassWord { get; set; } = null!;
+    public Array MovieGenre { get; set; } = null!;
 
-    [BsonElement("Poster")]
     public string Poster { get; set; } = null!;  
 }
