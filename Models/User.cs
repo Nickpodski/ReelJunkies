5c544ca227580ed5563b614f3f19dcd519d8c035
Nickpodski <Nickpodski@gmail.com>
2023-06-03 17:32:22 -0400

    User updates wasn't in last commit

--- Models/User.cs
+++ Models/User.cs
@@ -9,19 +9,14 @@ public class User
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    [BsonElement("UserName")]
     public string UserName { get; set; } = null!;
 
-    [BsonElement("Email")]
     public string Email { get; set; } = null!;
 
-    [BsonElement("PassWord")]
     public string PassWord { get; set; } = null!; 
 
-    [BsonElement("MoviesWatched")]
     public List<Movie> MoviesWatched { get; set; } = null!;
-    
-    [BsonElement("WatchList")]
+
     public List<Movie> WatchList { get; set; } = null!;
 
 }
